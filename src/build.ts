157acd5db0108c0de85e225cebaae5e13ecce4f0
cutls <web-pro@cutls.com>
2021-04-13 10:08:44 +0900

    add uncaughtException

--- src/build.ts
+++ src/build.ts
@@ -1,2 +1,7 @@
 import { execSync } from 'child_process'
+process.on('uncaughtException', function (err) {
+    console.warn(
+        'build native failed: skipped. cannot use itunes-nowplaying-mac'
+    )
+})
 if (process.platform === 'darwin') execSync('make')
